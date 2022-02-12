5487f664d4e208b8bebc278e36490d4ca42551e2
yuto90 <solidusgako4087@gmail.com>
2022-02-12 23:26:24 +0900

    クリア画面を作成

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -226,6 +226,51 @@ class MainPage extends StatelessWidget {
                           ),
                         )
                       : Text(''),
+                  // * ゲームクリア画面画面
+                  model.display == 'clear'
+                      ? Align(
+                          alignment: Alignment.center,
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text(
+                                'C L E A R !!!',
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.white),
+                              ),
+                              SizedBox(
+                                height: 180,
+                              ),
+                              OutlinedButton(
+                                onPressed: () {
+                                  //model.switchDisplay();
+                                  model.display = 'ready';
+                                  model.resetPosition();
+                                  model.reload();
+                                },
+                                child: Text(
+                                  'C O N T I N U E',
+                                  style: TextStyle(
+                                      fontSize: 20, color: Colors.white),
+                                ),
+                              ),
+                              OutlinedButton(
+                                onPressed: () {
+                                  //model.switchDisplay();
+                                  model.display = 'top';
+                                  model.resetPosition();
+                                  model.reload();
+                                },
+                                child: Text(
+                                  'E X I T',
+                                  style: TextStyle(
+                                      fontSize: 20, color: Colors.white),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      : Text(''),
                 ],
               ),
             ),
