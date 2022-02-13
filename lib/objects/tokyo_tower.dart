f28b2f4b59160807dcb1e64aae6d654f9e1e3091
yuto90 <solidusgako4087@gmail.com>
2022-02-13 15:18:01 +0900

    高層ビルと東京タワーのオブジェクトを追加

--- lib/objects/tokyo_tower.dart
+++ lib/objects/tokyo_tower.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-class Building extends StatelessWidget {
+class TokyoTower extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       //color: Colors.red,
       child: Image(
-        image: AssetImage('lib/images/billding.png'),
+        image: AssetImage('lib/images/tokyo_tower.png'),
         fit: BoxFit.cover,
         height: 200,
         width: 90,
